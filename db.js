86e45984f82fbddf5f3e17d0ce92cfa60a35b321
keekmx1 <kitthisack180897@gmail.com>
2023-07-20 21:22:09 +0700

    1

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2")
 const connection = mysql.createPool({
     connectionLimit: 10000,
-    host:"localhost",
-    user:"root",
-    password: "",
-    port: 3306,
+    host:"mysql-136683-0.cloudclusters.net",
+    user:"kee",
+    password: "12345678",
+    port: 18108,
     database:"moone_db"
 })
 
